642af466143ae396c3eb024b85ff9eec4a9f7a3c
gfrancinee <gludofrancine95@gmail.com>
2025-10-21 14:32:55 +0800

    teacher.php

--- teacher/teacher.php
+++ teacher/teacher.php
@@ -53,7 +53,7 @@ if ($currentUser) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Teacher homepage | ALS LMS</title>
+    <title>Teacher | ALS LMS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="css/teacher.css" />
